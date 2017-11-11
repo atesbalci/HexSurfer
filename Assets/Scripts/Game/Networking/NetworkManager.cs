7caf4a6723abeb1a5c82abb7cfd3c4fd64e7c288
Ateş Balcı <atesbalci@gmail.com>
2017-11-12 02:37:58 +0300

    basis for networking

---
+++ Assets/Scripts/Game/Networking/NetworkManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Game.Networking
+{
+    public class NetworkManager : Photon.MonoBehaviour
+    {
+        private void Start()
+        {
+            PhotonNetwork.ConnectUsingSettings(Application.version);
+        }
+    }
+}
