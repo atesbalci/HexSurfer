0afa6aa26d0044d841467e161c45eeb2743a22a1
Ateş Balcı <atesbalci@gmail.com>
2018-01-05 01:06:11 +0300

    Spawn indicator initials

---
+++ Assets/Scripts/Game/Utility/PeriodicRiser.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UniRx;
+using UnityEngine;
+
+namespace Game.Utility
+{
+    public class PeriodicRiser : MonoBehaviour
+    {
+        public float Period;
+        public float Duration;
+        public AnimationCurve Curve;
+        public float Radius;
+
+        private HexagonTiler _hex;
+        private float _timer;
+        private HexRiser _riser;
+
+        private void Start()
+        {
+            _timer = 0;
+            _hex = FindObjectOfType<HexagonTiler>();
+            _riser = new HexRiser { Active = false, Location = transform.position, Source = 0 , RiserCurve = Curve, Radius = Radius };
+        }
+
+        private void Update()
+        {
+            _timer += Time.deltaTime;
+            _riser.Location = transform.position;
+            if (_timer >= Period)
+            {
+                _timer -= Period;
+                _riser.Active = true;
+                Observable.Timer(TimeSpan.FromSeconds(Duration)).Subscribe(l => _riser.Active = false).AddTo(gameObject);
+                _hex.AddHexRiser(_riser);
+            }
+        }
+    }
+}
